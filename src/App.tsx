9a02a69aa8a5b65e7c50041acc88d05f9690d192
sienatrakay <jaylamarrayne123@gmail.com>
2026-04-02 00:41:35 -0300

    feat: re-render the Profile component when search a new user

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./screens/Home";
 import Profile from "./screens/Profile";
 
 function App() {
+  const location = useLocation();
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
+      <Route
+        path="/profile"
+        element={<Profile key={location.state?.user?.login} />}
+      />
     </Routes>
   );
 }
